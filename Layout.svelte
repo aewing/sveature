07808ffbae64559b201ac6105f4c71d5a3dcf2bc
Andrew Ewing <drew@phenocode.com>
2020-10-31 01:51:59 -0700

    v0.0.8 - Improved Layout and route error handling

--- Layout.svelte
+++ Layout.svelte
@@ -3,8 +3,10 @@
   export let brand = "sveature";
 
   let url = location.pathname;
+
   let component = null;
   let feature = null;
+  let params = [];
 
   function onClick(e) {
     if (e.target.nodeName === "A") {
@@ -38,12 +40,7 @@
   }
 
   $: {
-    const matches = [
-      ...url.matchAll(/\/component\/(.[^\/]+)(?:\/feature\/(.[^\/]+))?/g),
-    ];
-    if (matches && matches[0]) {
-      [[filename, component, feature]] = matches;
-    }
+    [component, feature = null, ...params] = url.substring(1).split("/");
   }
 </script>
 
@@ -60,18 +57,18 @@
   header {
     height: 64px;
     line-height: 64px;
-    background: var(--sveature-header-bg, hsl(260, 70%, 30%));
-    color: var(--sveature-header-fg, hsl(260, 70%, 97%));
+    background: var(--sveature-header-bg);
+    color: var(--sveature-header-fg);
     display: flex;
   }
   header .brand {
     width: 300px;
     text-align: center;
-    background: var(--sveature-brand-bg, hsl(260, 70%, 40%));
+    background: var(--sveature-brand-bg);
   }
   header .brand > a {
     text-decoration: none;
-    color: var(--sveature-brand-fg, hsl(260, 70%, 98%));
+    color: var(--sveature-brand-fg);
   }
   header .brand > a > h1 {
     margin: 0;
@@ -95,10 +92,12 @@
   main :global(hr) {
     margin-top: 16px;
     border: none;
-    border-top: 2px solid var(--sveature-hr-color, hsl(260, 10%, 90%));
+    border-top: 2px solid hsl(260, 10%, 90%);
   }
   .sidebar {
     width: 300px;
+    flex-grow: 0;
+    flex-shrink: 0;
     background: #eee;
   }
   .sidebar ul {
@@ -143,11 +142,44 @@
   }
   .sidebar a {
     text-decoration: none;
-    color: var(--sveature-link-color, hsl(260, 70%, 30%));
+    color: var(--sveature-link-color);
   }
   .content {
     padding: 8px 16px;
     width: 100%;
+    overflow: auto;
+  }
+
+  h2 {
+    font-size: 42px;
+    margin: 0;
+  }
+
+  h3 {
+    background: var(--sveature-section-bg);
+    color: var(--sveature-section-fg);
+    font-size: 32px;
+    padding: 8px 16px;
+    margin: 0;
+  }
+
+  .feature {
+    border: 2px solid var(--sveature-section-bg);
+    margin-bottom: 8px;
+  }
+
+  .feature__content {
+    padding: 8px 16px;
+  }
+
+  :root {
+    --sveature-section-bg: hsl(260, 70%, 40%);
+    --sveature-section-fg: white;
+    --sveature-link-color: hsl(260, 70%, 30%);
+    --sveature-header-bg: hsl(260, 70%, 30%);
+    --sveature-header-fg: hsl(260, 70%, 97%);
+    --sveature-brand-bg: hsl(260, 70%, 40%);
+    --sveature-brand-fg: hsl(260, 70%, 98%);
   }
 </style>
 
@@ -167,20 +199,18 @@
 <svelte:window
   on:click={onClick}
   on:pushstate={onPushState}
-  on:popstate={onPopState} />
+  on:popstate={onPopState}
+/>
 
 <main>
   <div class="sidebar">
     <ul>
       {#each Object.entries(components) as [component, cdef]}
         <li>
-          <a href={`/component/${component}`}>{component}</a>
+          <a href={`/${component}`}>{component}</a>
           <ul>
-            {#each Object.entries(cdef) as [feature, fdef]}
-              <li>
-                <a href={`/component/${component}/feature/${feature}`}>
-                  {fdef.title}</a>
-              </li>
+            {#each Object.keys(cdef) as feature}
+              <li><a href={`/${component}/${feature}`}>{feature}</a></li>
             {/each}
           </ul>
         </li>
@@ -188,17 +218,37 @@
     </ul>
   </div>
   <div class="content">
-    {#if component && feature}
+    {#if component && feature && components[component] && components[component][feature]}
       <svelte:component this={components[component][feature].Component} />
-    {:else if component}
+    {:else if component && !feature && components[component]}
       <h2>{component}</h2>
+      {#each Object.entries(components[component]) as [feature, fdef]}
+        <div class="feature">
+          <h3>{feature}</h3>
+          <div class="feature__content">
+            <svelte:component this={fdef.Component} />
+          </div>
+        </div>
+      {/each}
     {:else if !Object.keys(components).length}
       <h1>Oops!</h1>
       <p>No components were detected in your application.</p>
       <p>
         Double check your sveature.config.js and create some feature files using
-        Svelte (default pattern is src/**/features/*.svelte)
+        Svelte (the default feature file matching pattern is
+        src/**/features/*.svelte)
       </p>
+    {:else if !component & !feature}
+      <slot>
+        <h1>{brand}</h1>
+        <p>
+          Use the navigation to the left to preview components and their
+          features.
+        </p>
+      </slot>
+    {:else}
+      <h1>404</h1>
+      <p>The feature or component you're looking for was not found.</p>
     {/if}
   </div>
 </main>
